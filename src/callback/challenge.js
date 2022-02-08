3ffe1eae8d9204b250758051abc5017ef423d7be
saenzjulian <saenzjulian80@gmail.com>
2022-02-07 22:19:25 -0300

    call multiple times the rick and morty api

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,15 +1,10 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
-
-// A.P.I implementation with SMLHttpRequest 
+// A.P.I implementation with XMLHttpRequest 
 
 function fetchData(url_api, callback){ 
-    let xhttp = new XMLHttpRequest();
-    /* 
-    A nuestra referencia xhttp le pasamos un LLAMADO 'open'
-    donde: parametro1 = el metodo, parametro2 = la url,
-    parametro3 = verificación si es asincrono o no, valor por defecto true
-    */
+    let xhttp = new XMLHttpRequest(); 
     xhttp.open('GET', url_api, true); // true by default to async calls 
     xhttp.onreadystatechange = function (event){
         /* 
@@ -33,3 +28,16 @@ function fetchData(url_api, callback){
     // sending request
     xhttp.send();
 }
+
+fetchData(API, function(error1, data1){
+    if(error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2){
+        if(error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3){
+            if(error3) console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })
+})
